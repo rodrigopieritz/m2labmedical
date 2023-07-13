14a3d646bf1bf590b1a7ea74c7f4cfcff8eee5b4
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-07-13 20:38:14 -0300

    update

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,26 +1,45 @@
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const LoginComponent = () =>{
-    const navigate = useNavigate();
 
-    const redirectToLogin =() => {
-        navigate ('/home')
-    }
-    return(
-        <>
-        <form onSubmit={redirectToLogin}>
-            <legend>Login</legend>
+export const LoginComponent = () => {
+    const [data,setData] = useState ({
+        email:'',
+        password:'',
+    });
+  const navigate = useNavigate();
+
+  const handleInput = (event) => {
+    event.preventDefault();
+    const {value,id} = event.target;
+    setData ({...data,[id]: value})
+  }
+  const redirectToHome = () => {
+    navigate("/home");
+  };
+  return (
+    <>
+      <form onSubmit={redirectToHome}>
+        <legend>Login</legend>
         <div>
-            <label htmlFor="input-group">E-mail</label>
-            <input type="text" id="email" placeholder="Digite seu e-mail aqui" />
+          <label htmlFor="input-group">E-mail</label>
+          <input type="text" id="email" onInput = {handleInput} placeholder="Digite seu e-mail aqui" />
         </div>
         <div>
-            <label htmlFor="input-group">Senha</label>
-            <input type="password" id="password" placeholder="Digite sua senha aqui" />
+          <label htmlFor="input-group">Senha</label>
+          <input
+            type="password"
+            id="password"
+            onInput = {handleInput} placeholder="Digite sua senha aqui"
+          />
         </div>
-        <button type="submit">Logar</button>
+        <button type="submit" disabled={!data.email || !data.password}>Logar</button>
+
         </form>
-        </>
-    )
-
-}
+      <div>
+          <a href="">Esqueci minha senha</a>
+          <button>Criar Conta</button>
+        </div>
+    </>
+  );
+};
