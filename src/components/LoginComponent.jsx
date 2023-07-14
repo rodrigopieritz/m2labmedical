eec5fcc7aec300abd68a6ffd405f278ec6ce5bdd
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-07-13 21:02:03 -0300

    implantado alert ao clicar no reset de senha

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,45 +1,62 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 export const LoginComponent = () => {
-    const [data,setData] = useState ({
-        email:'',
-        password:'',
-    });
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
   const navigate = useNavigate();
 
   const handleInput = (event) => {
     event.preventDefault();
-    const {value,id} = event.target;
-    setData ({...data,[id]: value})
-  }
+    const { value, id } = event.target;
+    setData({ ...data, [id]: value });
+  };
+
   const redirectToHome = () => {
     navigate("/home");
   };
+
+  const handleForgotPassword = (event) => {
+    event.preventDefault();
+    const promptMessage = "Página em construção";
+    alert(promptMessage);
+  };
+
   return (
     <>
       <form onSubmit={redirectToHome}>
         <legend>Login</legend>
         <div>
           <label htmlFor="input-group">E-mail</label>
-          <input type="text" id="email" onInput = {handleInput} placeholder="Digite seu e-mail aqui" />
+          <input
+            type="text"
+            id="email"
+            onInput={handleInput}
+            placeholder="Digite seu e-mail aqui"
+          />
         </div>
         <div>
           <label htmlFor="input-group">Senha</label>
           <input
             type="password"
             id="password"
-            onInput = {handleInput} placeholder="Digite sua senha aqui"
+            onInput={handleInput}
+            placeholder="Digite sua senha aqui"
           />
         </div>
-        <button type="submit" disabled={!data.email || !data.password}>Logar</button>
-
-        </form>
+        <button type="submit" disabled={!data.email || !data.password}>
+          Logar
+        </button>
+      </form>
       <div>
-          <a href="">Esqueci minha senha</a>
-          <button>Criar Conta</button>
-        </div>
+        <a href="#" onClick={handleForgotPassword}>
+          Esqueci minha senha
+        </a>
+        <button>Criar Conta</button>
+      </div>
     </>
   );
 };
