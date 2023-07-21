adaa660c9adda02d6c89e2792e8bedcaceb15b67
Rodrigo Pieritz <rodrigopieritz@gmail.com>
2023-07-21 13:10:11 -0300

    1st commit - component created

---
+++ src/components/MedicalRegister/MedicalRegister.jsx
@@ -0,0 +1,6 @@
+export const MedicalRegisterComponent = () => {
+
+    return(
+
+    <p>Medical Register Component is render</p>)
+}
--- src/pages/PatientRegister/PatientRegister.pages.jsx
+++ src/pages/PatientRegister/PatientRegister.pages.jsx
@@ -1,53 +1,55 @@
-// import { useContext } from "react";
-// import { AuthContext } from "../../context/auth/auth.context";
-// import { Navigate } from "react-router";
-// import SidebarMenu from "../../components/SidebarMenu/SidebarMenu";
-// import Toolbar from "../../components/Toolbar/ToolbarComponent";
-// import * as Styled from "../../global.style";
-// import { PatientRegister } from "../../components/PatientRegister/PatientRegister";
-
-// export const PatientRegisterPage = () => {
-//   const { auth } = useContext(AuthContext);
-
-//   const render = () => {
-//     const userEmail = auth.user.email;
-//     const userName = userEmail.split("@")[0];
-//     return (
-//       <Styled.PageContainer>
-//         <SidebarMenu />
-//         <Styled.MainContent>
-//           <Toolbar
-//             pageTitle="Cadastrar Paciente"
-//             userName={userName}
-//             userPhoto="userPhoto.png"
-//           />
-//           <PatientRegister />
-//         </Styled.MainContent>
-//       </Styled.PageContainer>
-//     );
-//   };
-//   return auth.isLogged ? render() : <Navigate to={"/login"}/>
-// };
-
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth/auth.context";
+import { Navigate } from "react-router";
 import SidebarMenu from "../../components/SidebarMenu/SidebarMenu";
 import Toolbar from "../../components/Toolbar/ToolbarComponent";
 import * as Styled from "../../global.style";
 import { PatientRegister } from "../../components/PatientRegister/PatientRegister";
 
 export const PatientRegisterPage = () => {
- 
+  const { auth } = useContext(AuthContext);
+
+  const render = () => {
+    const userEmail = auth.user.email;
+    const userName = userEmail.split("@")[0];
     return (
       <Styled.PageContainer>
         <SidebarMenu />
         <Styled.MainContent>
           <Toolbar
             pageTitle="Cadastrar Paciente"
-            userName="Página de teste"
+            userName={userName}
             userPhoto="userPhoto.png"
           />
           <PatientRegister />
         </Styled.MainContent>
       </Styled.PageContainer>
     );
-  }
+  };
+  return auth.isLogged ? render() : <Navigate to={"/login"}/>
+};
+
+// Código para rodar ao fazer manutneção (dispensa a autendicação de usuário)
+
+// import SidebarMenu from "../../components/SidebarMenu/SidebarMenu";
+// import Toolbar from "../../components/Toolbar/ToolbarComponent";
+// import * as Styled from "../../global.style";
+// import { PatientRegister } from "../../components/PatientRegister/PatientRegister";
+
+// export const PatientRegisterPage = () => {
+ 
+//     return (
+//       <Styled.PageContainer>
+//         <SidebarMenu />
+//         <Styled.MainContent>
+//           <Toolbar
+//             pageTitle="Cadastrar Paciente"
+//             userName="Página de teste"
+//             userPhoto="userPhoto.png"
+//           />
+//           <PatientRegister />
+//         </Styled.MainContent>
+//       </Styled.PageContainer>
+//     );
+//   }
  
